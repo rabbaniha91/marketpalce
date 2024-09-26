071e202c255140f9ae21385107f70bec2133ddb6
rabbaniha <hrabbaniha@gmail.com>
2024-09-26 17:41:55 +0330

    create address model

---
+++ services/user_services/models/address.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const addressSchema = mongoose.Schema({
+  province: { type: String, required: true },
+  township: { type: String, required: true },
+  city: { type: String, required: true },
+  details: { type: String, required: true },
+});
+
+const Address = mongoose.model("Address", addressSchema);
+module.exports = Address;
