84b7216bb2120af018febc80872daa0af183e8cc
rabbaniha <hrabbaniha@gmail.com>
2024-09-29 16:08:31 +0330

    create notification model

--- services/user_services/controllers/user.js
+++ services/user_services/controllers/user.js
@@ -10,31 +10,39 @@ const { createToken } = require("../../../configs/createJWT");
 const { secretAccessToken, secretRefreshToken } = require("../../../configs/env_vars");
 
 class userController {
+  // register users with email and password
   static register = catchFunc(async (req, res, next) => {
     try {
-      await UserModel.deleteMany({});
       const { email, password } = req.body;
 
+      //   validate user requests with express validator
       const validateResult = validationResult(req);
 
       if (!validateResult.isEmpty()) {
         return next(new AppError(validateResult.array()[0].msg, 400));
       }
 
+      //   check repeat email
       let user = await User.getUserByEmail(email);
 
       if (user) {
         return next(new AppError("The user with email address already exists", 400));
       }
 
+      //   hashed password
       const salt = await bcrypt.genSalt(10);
       const hashPass = await bcrypt.hash(password, salt);
 
+      //   create user
       user = await User.createUser({ email, password: hashPass });
+
+      //   crate necessary tokens
       const refreshToken = createToken(user._id, secretRefreshToken, "15d");
       const accessToken = createToken(user._id, secretAccessToken, "2h");
       user.refreshTokens.push(refreshToken);
       await user.save();
+
+      //   send response
       res.status(201).json({
         message: "User successfuly created",
         user,
---
+++ services/user_services/models/notification.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  user: { type: mongoose.Types.ObjectId, ref: "User" },
+  status: { type: String, enum: ["read", "unread"] },
+});
+
+const Notification = mongoose.model("Notification", notificationSchema);
+module.exports = Notification;
